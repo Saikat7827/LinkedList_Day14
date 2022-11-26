bdf39fa8c51901ea3bb41e51636aea08d43144b2
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-26 22:50:54 +0530

    Added: 30 in between 56 and 70

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 package com.bridgelabz;
 
 public class Main {
-	 public static void main(String[] args) {
+	  public static void main(String[] args) {
 
 		    LinkedList<Integer> linkedL = new LinkedList<>();
 
-		    linkedL.addFirst(70);
-		    linkedL.addFirst(30);
-		    linkedL.addFirst(56);
+		    linkedL.addLast(56);
+		    linkedL.addLast(30);
+		    linkedL.addLast(70);
 
 		    linkedL.display();
 		  }
 
-
+		
 }
