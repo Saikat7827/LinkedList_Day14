61c63be03867cafdd6473a143e0cb45fc515ce6e
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-26 20:59:46 +0530

    Added:No 56,30 and 70 as a LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
-public class LinkedList {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List Program");
+public class LinkedList<E> {
+	  Node<E> head;
+
+	  public void addLast(E item) {
+	    Node<E> newNode = new Node<>(item);
+	    if (head == null) head = newNode;//node being added is the first node
+	    else if (head.next == null) head.next = newNode;  //node being added is the second node
+	    else {
+	      Node<E> temp = head;
+	      while (temp.next != null) temp = temp.next; //traverse to the last node
+	      temp.next = newNode;
+	    }
+	  }
+
 	}
+
+	class Node<E> {
+
+	  E data;
+	  Node<E> next;
+
+	  Node(E data) {
+	    this.data = data;
+	  }
 }
---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Main {
+	 public static void main(String[] args) {
+
+		    LinkedList<Integer> linkedL = new LinkedList<>();
+
+		    linkedL.addLast(56);
+		    linkedL.addLast(30);
+		    linkedL.addLast(70);
+		  }
+
+}
